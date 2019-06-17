3176bfeff647fb743414379bf3fd35a48df0283e
Richard Grey <richard@secondgenome.com>
2019-06-16 21:32:08 -0700

    Filter instances by project.  Also, stop and start instances.

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -4,11 +4,7 @@ import click
 session = boto3.Session(profile_name='rg_sand', region_name='us-west-2')
 ec2 = session.resource('ec2')
 
-@click.command()
-@click.option('--project', default=None,
-	help="Only instances for project (tag Project:<name>)")
-def list_intstances(project):
-	"List EC2 instances"
+def filter_instances(project):
 	instances = []
 
 	if project:
@@ -16,6 +12,21 @@ def list_intstances(project):
 		instances = ec2.instances.filter(Filters=filters)
 	else:
 		instances = ec2.instances.all()
+
+	return instances
+
+@click.group()
+def instances():
+	"""Commands for instances"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+	help="Only instances for project (tag Project:<name>)")
+def list_intstances(project):
+	"List EC2 instances"
+
+	instances = filter_instances(project)
+
 	for i in instances:
 		tags = { t['Key']: t['Value'] for t in i.tags or [] }
 		print(', '.join((
@@ -29,6 +40,33 @@ def list_intstances(project):
 
 	return
 
+@instances.command('stop')
+@click.option('--project', default=None,
+	help='Only instances for project')
+def stop_instances(project):
+	"Stop EC2 instances"
+
+	instances = filter_instances(project)
+
+	for i in instances:
+		print("Stopping {0}...".format(i.id))
+		i.stop()
+
+	return
+
+@instances.command('start')
+@click.option('--project', default=None,
+	help='Only instances for project')
+def start_instances(project):
+	"Start EC2 instances"
+
+	instances = filter_instances(project)
+
+	for i in instances:
+		print("Starting {0}...".format(i.id))
+		i.start()
+
+	return
 
 if __name__ == '__main__':
-	list_intstances()
+	instances()
